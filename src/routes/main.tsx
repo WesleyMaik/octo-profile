b5cded1469595856f9e2676406ef8dbf2d06a0ef
WesleyMaik <wesleynamikaze404@gmail.com>
2023-02-15 14:55:20 -0400

    :sparkles: feat add global styles

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
+import { Providers } from '../components/Providers'
+
+import "../style/global.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
 )
