341064e08b1fa8b3faba1aee7ac874c1be7f4756
WesleyMaik <wesleynamikaze404@gmail.com>
2023-02-16 17:31:13 -0400

    :sparkles: feat add Header component to Main

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 import { Providers } from '../components/Providers'
+import { Header } from '../components/Header';
 
 import "../style/global.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Providers>
+      <Header />
       <App />
     </Providers>
   </React.StrictMode>,
-)
+);
