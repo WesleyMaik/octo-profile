9cddad1ce4c3593fef79faade284dd7a14791e6e
WesleyMaik <wesleynamikaze404@gmail.com>
2023-02-17 17:28:25 -0400

    :sparkles: feat add App Component

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,6 +1,29 @@
+//Modules
+import { useQueryURL } from "../hooks/useQueryURL";
+
+//Component
+import { Profile } from "../components/Profile";
+import { SignIn } from "../components/SignIn";
+
 const App = () => {
+  const url = useQueryURL();
+  const username = url.get("user") || "";
+
+  const isProfile = Boolean(username);
+  const handleSearchProfile = (username: string) => {
+    window.location.search += `?user=${username}`
+  };
+
   return (
-    <></>
+    (!isProfile) ? (
+      <SignIn
+        onSubmit={handleSearchProfile}
+      />
+    ) : (
+      <Profile
+        username={username}
+      />
+    )
   );
 };
 
