0153640216aa2b842de4b5b0a9261facca3a5072
WesleyMaik <wesleynamikaze404@gmail.com>
2023-02-17 17:23:33 -0400

    :sparkles: feat add Toggle Theme button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,30 @@
-import { GitHub } from "react-feather";
-import { HStack, IconButton, Link } from "@chakra-ui/react";
+import { GitHub, Sun, Moon } from "react-feather";
+import { HStack, IconButton, Link, useColorMode } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 export const Header = () => {
+    const { colorMode, toggleColorMode, setColorMode } = useColorMode(),
+        isLight = colorMode == "light" ? Moon : Sun;
+
+    useEffect(() => {
+        setColorMode("dark");
+    }, []);
+
     return (
         <HStack
             width="full"
             justifyContent="flex-end"
+            spacing={0}
         >
+            <IconButton
+                as={isLight}
+                cursor="pointer"
+                aria-label="Toggle Mode Icon"
+                rounded="full"
+                padding={2}
+                margin={2}
+                onClick={toggleColorMode}
+            />
             <Link
                 href="https://github.com/WesleyMaik/octo-profile"
                 target="_blank"
